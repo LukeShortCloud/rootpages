3931bb479f9a685f1b5d278ef8bac17fe672bbe4
ekultails <ekultails@gmail.com>
2017-12-29 16:22:19 -0500

    Removed old build scripts and added documentation on how to use Sphinx.

--- reinstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./uninstall.sh
-./install.sh
--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# remove the export line ending in "rp/bin",
-# backing up the .bashrc as .bashrc_rp_uninstall
-sed -i'_rp_uninstall' -e '/rp\/bin$/d' ${HOME}/.bashrc
-# delete the installation directory
-rm -rf ${HOME}/rp
