08d0b6d179208da1b80ea6755f74074b92dbcb78
Luke Short <ekultails@gmail.com>
2019-07-02 11:43:35 -0400

    Start of 2019.10.01-dev.

--- src/conf.py
+++ src/conf.py
@@ -57,7 +57,7 @@ author = 'Luke Short'
 # built documents.
 #
 # The short X.Y version.
-version = '2019.07.01'
+version = '2019.10.01-dev'
 # The full version, including alpha/beta/rc tags.
 #release = ''
 
