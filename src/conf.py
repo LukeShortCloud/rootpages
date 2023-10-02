bc450d13d5937666a010c1fb8fd6d2f1b25d6119
Luke Short <ekultails@gmail.com>
2023-10-01 22:00:18 -0600

    Release 2023.10.01

--- src/conf.py
+++ src/conf.py
@@ -57,7 +57,7 @@ author = 'Luke Short'
 # built documents.
 #
 # The short X.Y version.
-version = '2023.10.01-dev'
+version = '2023.10.01'
 # The full version, including alpha/beta/rc tags.
 #release = ''
 
