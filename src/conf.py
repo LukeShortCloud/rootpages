c63b75dfce07e0ed7eef9596993bfa0d3f142107
Luke Short <ekultails@gmail.com>
2025-01-05 15:37:25 -0700

    Start of 2025.04.01-dev

--- src/conf.py
+++ src/conf.py
@@ -57,7 +57,7 @@ author = 'Luke Short'
 # built documents.
 #
 # The short X.Y version.
-version = '2025.01.01'
+version = '2025.04.01-dev'
 # The full version, including alpha/beta/rc tags.
 #release = ''
 
